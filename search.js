5bdaba640ffbefb552051a4b6f7e17de6a13f4ee
Lohith Raj <lohith.raj@ellucian.com>
2020-02-06 11:40:19 +0530

    update 2

--- search.js
+++ search.js
@@ -1,12 +1,23 @@
 angular.module('myApp',[])
-       .controller('appController',function($scope,$http){
-            $scope.apiCall = function(){
-                $http({
-                    url: 'http://www.omdbapi.com/?apikey=f765e195&s='+ $scope.searchText,
-                    method: "GET"
-                }).then(
-                    function(response) {
-                        $scope.movieData = response.data.Search;
-                    })
+       .controller('appController',function($scope,$http,$timeout){
+            $scope.apiCall = function() {
+                if($scope.searchText.length >= 3)
+                {
+                    $timeout(function(){
+                        $http({
+                            url: 'http://www.omdbapi.com/?apikey=f765e195&s='+ $scope.searchText,
+                            method: "GET"
+                        }).then(
+                            function(response) {
+                                $scope.movieData = response.data.Search;
+                            })
+                        },1000)
                 }
+
+       }
+            $scope.showFilterOptions = function(){
+                $scope.showFilter = ! $scope.showFilter
+            }
+            $scope.showFilter = false
+            $scope.sort = "Title"
             })
