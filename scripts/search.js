995e8f4baa947f2a7ca1f4667dbaddae72159b51
Lohith Raj <lohith.raj@ellucian.com>
2020-02-10 12:01:25 +0530

    filters

--- scripts/search.js
+++ scripts/search.js
@@ -1,18 +1,30 @@
-angular.module('myApp',[])
+angular.module('myApp',['ngRoute'])
        .controller('appController',function($scope,$http,$timeout){
             $scope.pageNo = 1
             $scope.showFilter = false
             $scope.reverse = false
             $scope.sortValue = "Title"
             $scope.totalPages = 1
+            $scope.filterButtonClicked = false
+            $scope.filters = {}
+            $scope.types = ["movie","series","episode"]
+            $scope.years = []
+            for(let i=1990;i<=2020;i++){
+                $scope.years.push(i)
+            }
             $scope.apiCall = function() {
 
                 if($scope.searchText.length >= 3)
                 {
-                        console.log('Api call with ', $scope.pageNo);
-                        
+                        var url = 'http://www.omdbapi.com/?apikey=f765e195&s='+ $scope.searchText + '&page=' + $scope.pageNo
+                        if($scope.filters.type){
+                            url = url+'&type='+ $scope.filters.type
+                        }
+                        if($scope.filters.year){
+                            url = url+"&y="+ $scope.filters.year
+                        }                         
                         $http({
-                            url: 'http://www.omdbapi.com/?apikey=f765e195&s='+ $scope.searchText + '&page=' + $scope.pageNo,
+                            url: url,
                             method: "GET"
                         }).then(
                             function(response) {
@@ -46,3 +58,15 @@ angular.module('myApp',[])
             $scope.apiCall();
         }
     })
+    .config(function($routeProvider) {
+        $routeProvider
+        .when("/home", {
+          templateUrl : "home.html"
+        })
+        .when("/about", {
+          templateUrl : "about.html"
+        })
+        .when("/contact-us", {
+          templateUrl : "contact.html"
+        });
+      })
